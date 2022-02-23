3fffe01407240988fd2163f55f3213829c440259
anonymous anonymous <ashnavbhatia@gmail.com>
2022-02-23 23:05:24 +0000

    Whitelist columns in Restaurant for ActiveAdmin

--- app/admin/restaurants.rb
+++ app/admin/restaurants.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Restaurant do
 
+ permit_params :dish_id, :address, :neighborhood_id, :google_maps_url
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
